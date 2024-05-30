1587dfe31f1b19c73cc5f28d475f6d5c0324caa9
Dcarreon <dcarreonr@gmail.com>
2024-05-30 17:20:19 -0500

    Player falling is now handled in the main player script rather than the move state

--- scripts/PlayerMoveState.cs
+++ scripts/PlayerMoveState.cs
@@ -12,9 +12,6 @@ public partial class PlayerMoveState : State
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
     bool StateEnabled = false;
 
     public override void _Ready()
@@ -35,17 +32,13 @@ public partial class PlayerMoveState : State
         if (StateEnabled) {
             Vector2 velocity = Player.Velocity;
 
-            // Add the gravity.
-            if (!Player.IsOnFloor())
-                velocity.Y += gravity * (float)delta;
-
             // Handle Jump.
             if (Input.IsActionJustPressed("ui_accept") && Player.IsOnFloor())
                 velocity.Y = JumpVelocity;
 
             // Get the input direction and handle the movement/deceleration.
             // As good practice, you should replace UI actions with custom gameplay actions.
-            Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+            Vector2 direction = Input.GetVector("left", "right", "ui_up", "ui_down");
             if (direction != Vector2.Zero)
             {
                 velocity.X = direction.X * Speed;
--- scripts/player.cs
+++ scripts/player.cs
@@ -5,6 +5,7 @@ public partial class player : CharacterBody2D
 {
 	StateMachine PlayerStateMachine;
 	PlayerMoveState MoveState;
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
     public override void _Ready()
     {
@@ -14,5 +15,10 @@ public partial class player : CharacterBody2D
 
     public override void _PhysicsProcess(double delta)
 	{
+            if (!IsOnFloor()) {
+				Vector2 velocity = Velocity;
+                velocity.Y += gravity * (float)delta;
+				Velocity = velocity;
+			}
 	}
 }
