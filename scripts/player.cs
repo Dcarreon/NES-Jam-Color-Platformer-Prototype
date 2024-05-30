b6cee20333e59b5e5325cb579e86e5c30592d180
Dcarreon <dcarreonr@gmail.com>
2024-05-30 14:29:07 -0500

    Implemented default player controls and implemented player into the main game level

--- scripts/player.cs
+++ scripts/player.cs
@@ -3,13 +3,19 @@ using System;
 
 public partial class player : CharacterBody2D
 {
+	StateMachine PlayerStateMachine;
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
+    public override void _Ready()
+    {
+		PlayerStateMachine = GetNode<StateMachine>("StateMachine");
+    }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
