eae4c3c2fe6d64bb7a66c0329d8736601ba00317
Dcarreon <dcarreonr@gmail.com>
2024-05-31 06:46:24 -0500

    Replaced IsEnabled bool to SetPhysicsProcess

--- scripts/PlayerMoveState.cs
+++ scripts/PlayerMoveState.cs
@@ -12,58 +12,56 @@ public partial class PlayerMoveState : State
 	public const float Speed = 150.0f;
 	public const float JumpVelocity = -400.0f;
 
-    bool StateEnabled = false;
-
     public override void _Ready()
     {
+        SetPhysicsProcess(false);
     }
 
-    public override void EnterState() {
-        StateEnabled = true;
+    public override void EnterState() 
+    {
+        SetPhysicsProcess(true);
     }
 
     public override void ExitState()
     {
-        StateEnabled = false;
+        SetPhysicsProcess(false);
     }
 
     public override void _PhysicsProcess(double delta)
 	{
-        if (StateEnabled) {
-            Vector2 velocity = Player.Velocity;
+        Vector2 velocity = Player.Velocity;
 
-            // Handle Jump.
-            if (Input.IsActionJustPressed("ui_accept") && Player.IsOnFloor())
-                velocity.Y = JumpVelocity;
+        // Handle Jump.
+        if (Input.IsActionJustPressed("ui_accept") && Player.IsOnFloor())
+            velocity.Y = JumpVelocity;
 
-            // Get the input direction and handle the movement/deceleration.
-            // As good practice, you should replace UI actions with custom gameplay actions.
-            Vector2 direction = Input.GetVector("left", "right", "ui_up", "ui_down");
-            if (direction != Vector2.Zero)
-            {
-                velocity.X = direction.X * Speed;
-            }
-            else
-            {
-                velocity.X = Mathf.MoveToward(Player.Velocity.X, 0, Speed);
-            }
-
-            if (direction.X > 0) {
-                Player.AnimatedSprite.FlipH = false;
-            }
-            else if (direction.X < 0) {
-                Player.AnimatedSprite.FlipH = true;
-            }
-
-            if (direction.X == 0) {
-                Player.AnimatedSprite.Play("idle");
-            }
-            else {
-                Player.AnimatedSprite.Play("run");
-            }
-
-            Player.Velocity = velocity;
-            Player.MoveAndSlide();
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 direction = Input.GetVector("left", "right", "ui_up", "ui_down");
+        if (direction != Vector2.Zero)
+        {
+            velocity.X = direction.X * Speed;
         }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Player.Velocity.X, 0, Speed);
+        }
+
+        if (direction.X > 0) {
+            Player.AnimatedSprite.FlipH = false;
+        }
+        else if (direction.X < 0) {
+            Player.AnimatedSprite.FlipH = true;
+        }
+
+        if (direction.X == 0) {
+            Player.AnimatedSprite.Play("idle");
+        }
+        else {
+            Player.AnimatedSprite.Play("run");
+        }
+
+        Player.Velocity = velocity;
+        Player.MoveAndSlide();
 	}
 }
